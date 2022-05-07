c399d35626fcb571e4f2c1435b48c64e1aae9e0d
baolan2005 <baolan2005@gmail.com>
2022-05-07 22:05:26 +0200

    init project

---
+++ DemoWebViewSDK/DemoWebViewSDKTests/DemoWebViewSDKTests.m
@@ -0,0 +1,36 @@
+//
+//  DemoWebViewSDKTests.m
+//  DemoWebViewSDKTests
+//
+//  Created by Lan Le on 07.05.22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoWebViewSDKTests : XCTestCase
+
+@end
+
+@implementation DemoWebViewSDKTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
