3be7a3ee4c9e20eff1dd166e65874f63f8fcb191
baolan2005 <baolan2005@gmail.com>
2022-05-08 15:20:14 +0200

    update built framework

---
+++ DemoWebViewSDK.framework/Headers/DemoWebViewSDK.h
@@ -0,0 +1,18 @@
+//
+//  DemoWebViewSDK.h
+//  DemoWebViewSDK
+//
+//  Created by Lan Le on 07.05.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoWebViewSDK.
+FOUNDATION_EXPORT double DemoWebViewSDKVersionNumber;
+
+//! Project version string for DemoWebViewSDK.
+FOUNDATION_EXPORT const unsigned char DemoWebViewSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoWebViewSDK/PublicHeader.h>
+#import <DemoWebViewSDK/MTDController.h>
+
